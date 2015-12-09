67b0cb1b6f1f63c5826e860fb67c8269835511d7
nhphuc <nhphuc@mytrax.co.jp>
2015-12-09 13:17:01 +0700

    Change hot-blog attribute

--- application/views/blogs/index.php
+++ application/views/blogs/index.php
@@ -8,8 +8,8 @@ import('css/blogs/header');
         </paper-toolbar>
         <blog-search placeholder="Tim kiem"></blog-search>
         <hot-blog
-            src="http://img.v3.news.zdn.vn/w660/Uploaded/nutmjz/2015_12_02/12319798_10204788201299254_1637655560_n.jpg"
-            href="http://news.zing.vn/Doi-chan-phi-thuong-cua-co-gai-Viet-chinh-phuc-sa-mac-post606705.html">
+            thumb="http://img.v3.news.zdn.vn/w660/Uploaded/nutmjz/2015_12_02/12319798_10204788201299254_1637655560_n.jpg"
+            src="http://news.zing.vn/Doi-chan-phi-thuong-cua-co-gai-Viet-chinh-phuc-sa-mac-post606705.html">
             <div class="title">Đôi chân phi thường của cô gái Việt chinh phục sa mạc</div>
             <div class="description">Để vượt sa mạc Atacama - nơi có độ cao 3.200 m so với mực nước biển, Vũ Phương Thanh, sinh năm 1990 đã chạy, đi bộ 85-115km/tuần, rèn đôi chân hoạt động liên tục 12 tiếng/ngày.</div>
         </hot-blog>
