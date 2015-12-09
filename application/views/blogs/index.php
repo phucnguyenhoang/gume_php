4d22896d7f49400ff06e2b0a4dcd9d88bbfcb028
nhphuc <nhphuc@mytrax.co.jp>
2015-12-09 14:45:12 +0700

    make blog menu

--- application/views/blogs/index.php
+++ application/views/blogs/index.php
@@ -13,6 +13,28 @@ import('css/blogs/header');
             <div class="title">Đôi chân phi thường của cô gái Việt chinh phục sa mạc</div>
             <div class="description">Để vượt sa mạc Atacama - nơi có độ cao 3.200 m so với mực nước biển, Vũ Phương Thanh, sinh năm 1990 đã chạy, đi bộ 85-115km/tuần, rèn đôi chân hoạt động liên tục 12 tiếng/ngày.</div>
         </hot-blog>
+        <paper-menu class="blog-menu">
+            <paper-icon-item>
+                <div class="avatar red" item-icon></div>
+                <div class="flex">Alphonso</div>
+                <iron-icon icon="communication:chat"></iron-icon>
+            </paper-icon-item>
+            <paper-icon-item>
+                <div class="avatar orange" item-icon></div>
+                <div class="flex">Andrews</div>
+                <iron-icon icon="communication:chat"></iron-icon>
+            </paper-icon-item>
+            <paper-icon-item>
+                <div class="avatar green" item-icon></div>
+                <div class="flex">Angela</div>
+                <iron-icon icon="communication:chat"></iron-icon>
+            </paper-icon-item>
+            <paper-icon-item>
+                <div class="avatar blue" item-icon></div>
+                <div class="flex">Lorem</div>
+                <iron-icon icon="communication:chat"></iron-icon>
+            </paper-icon-item>
+        </paper-menu>
     </div>
     <paper-header-panel main mode="waterfall">
         <paper-toolbar class="paper-header">
